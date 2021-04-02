caf6aff764f9dadc532a6bdb9864e91557421625
Sojung Choi <sojc312gmail.com>
2021-04-02 13:03:56 -0700

    updated front end to serve react app from express

--- server/express.js
+++ server/express.js
@@ -13,6 +13,12 @@ import userRoutes from './routes/user.routes'
 /* modules for server side rendering */
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
+import MainRouter from './../client/MainRouter'
+import { StaticRouter } from 'react-router-dom'
+
+import {ServerStyleSheets, ThemeProvider} from '@material-ui/styles'
+import theme from './../client/theme'
+//end
 
 /* comment out before building for production */
 import devBundle from './devBundle'
@@ -40,11 +46,38 @@ app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 /* mount routes */
 app.use('/', userRoutes)
 
-app.get('/', (req, res) => {
-    res.status(200).send(Template())
+app.get('*', (req, res) => {
+    const sheets = new ServerStyleSheets()
+    const context={}
+    const markup = ReactDOMServer.renderToString(
+        sheets.collect(
+            <StaticRouter location={req.url} context={context}>
+                <ThemeProvider theme={theme}>
+                    <MainRouter />
+                </ThemeProvider>
+            </StaticRouter>
+        )
+    )
+    if (context.url){
+        return res.redirect(303, context.url)
+    }
+    const css = sheets.toString()
+    res.status(200).send(Template({
+        markup: markup,
+        css: css
+    }))
 })
 
 
 /* catch unauthorized errors */
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).json({"error" : err.name + ": " + err.message})
+    }else if (err) {
+      res.status(400).json({"error" : err.name + ": " + err.message})
+      console.log(err)
+    }
+})
+
 
 export default app
