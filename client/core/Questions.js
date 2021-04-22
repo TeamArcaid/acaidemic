59b41e3633a807187ba640bb988303042242d448
Peter R <pryzanych@gmail.com>
2021-04-22 16:28:57 -0700

    Remove unsused imports

--- client/core/Questions.js
+++ client/core/Questions.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, CardActions, ButtonGroup, SvgIcon, Avatar } from '@material-ui/core';
+import { Button, CardActions, ButtonGroup } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
