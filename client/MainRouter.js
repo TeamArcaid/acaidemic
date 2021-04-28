3f8761de8ba09685ace5c8cbcf89961e402edc54
Sojung Choi <sojc312gmail.com>
2021-04-27 18:28:52 -0700

    minor edits again

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -4,13 +4,6 @@ import Home from './core/Home'
 import Menu from './core/Menu'
 import Garden from './core/Garden'
 import Questions from './core/Questions'
-/*import Users from './user/Users'
-import Signup from './user/Signup'
-import Signin from './auth/Signin' 
-import EditProfile from './user/EditProfile'
-import Profile from './user/Profile'
-import PrivateRoute from './auth/PrivateRoute'
-import Menu from './core/Menu' */
 
 
 const MainRouter = () => {
