5ce2e0d7e65de7ee595a6eda86864d350ca5f4bb
Peter R <pryzanych@gmail.com>
2021-04-28 19:28:19 -0700

    Add Home page styling and button colors

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,18 +1,19 @@
-import React from "react"
-import { Button, CardActions } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
-import Card from "@material-ui/core/Card"
-import CardContent from "@material-ui/core/CardContent"
-import CardMedia from "@material-ui/core/CardMedia"
-import Typography from "@material-ui/core/Typography"
-import { Link } from "react-router-dom"
-import landing from "../assets/images/Backgrounds/01 Artwork.svg"
+import React from 'react';
+import { Button, CardActions } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import landing from '../assets/images/Backgrounds/01 Artwork.svg';
 
 const useStyles = makeStyles((theme) => ({
   card: {
     maxWidth: 600,
-    margin: "auto",
+    margin: 'auto',
     marginTop: theme.spacing(5),
+    backgroundColor: '#A8D9C4',
   },
   title: {
     padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
@@ -21,26 +22,29 @@ const useStyles = makeStyles((theme) => ({
   media: {
     minHeight: 400,
   },
-}))
+  button: {
+    backgroundColor: '#FF8576',
+    color: 'white',
+    fontWeight: 'bold',
+  },
+}));
 
 export default function Home() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <Card className={classes.card}>
-      <Typography variant="h6" className={classes.title}>
-        SAGE 
+      <Typography variant="h1" className={classes.title} align="center">
+        SAGE
       </Typography>
       <CardMedia className={classes.media} image={landing} title="splash garden" />
       <CardContent>
-        <Typography variant="body2" component="p">
-    
-        </Typography>
+        <Typography variant="body2" component="p"></Typography>
       </CardContent>
       <CardActions>
-        <Button variant="contained" component={Link} to="/menu">
+        <Button className={classes.button} variant="contained" component={Link} to="/menu">
           New Plant
         </Button>
       </CardActions>
     </Card>
-  )
+  );
 }
--- client/core/Menu.js
+++ client/core/Menu.js
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from "react"
-import { Button, CardActions, TextField, Slider } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
-import Card from "@material-ui/core/Card"
-import CardContent from "@material-ui/core/CardContent"
-import Typography from "@material-ui/core/Typography"
-import { Link } from "react-router-dom"
-import {create} from '../plant/api-plant.js'
-
+import React, { useEffect, useState } from 'react';
+import { Button, CardActions, TextField, Slider, FormControl } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import { create } from '../plant/api-plant.js';
 
 const useStyles = makeStyles((theme) => ({
   card: {
     maxWidth: 600,
-    margin: "auto",
+    margin: 'auto',
     marginTop: theme.spacing(5),
   },
   title: {
@@ -19,40 +18,45 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.openTitle,
   },
   name: {
-    margin: "auto",
+    margin: 'auto',
   },
-}))
+  button: {
+    backgroundColor: '#FF8576',
+    color: 'white',
+    fontWeight: 'bold',
+  },
+}));
 
 export default function menu() {
-  const classes = useStyles()
-  const [feeling, setFeeling] = useState(50)
+  const classes = useStyles();
+  const [feeling, setFeeling] = useState(50);
 
-  const [values, setValues]=useState({
+  const [values, setValues] = useState({
     problem: '',
-    error: ''
-  })
+    error: '',
+  });
 
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value})
-  }
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
 
-  const clickSubmit=()=> {
-    console.log('clickSubmit function called')
+  const clickSubmit = () => {
+    console.log('clickSubmit function called');
     const plant = {
-      problem: values.problem || undefined
-    }
-    create(plant).then((data)=> {
-      if(data.error){
-        setValues({ ...values, error: data.error})
+      problem: values.problem || undefined,
+    };
+    create(plant).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
       } else {
-        setValues({ ...values, error:''})
+        setValues({ ...values, error: '' });
       }
-    })
-  }
+    });
+  };
 
   const handleFeelingChange = (event, newValue) => {
-    setFeeling(newValue)
-  }
+    setFeeling(newValue);
+  };
 
   return (
     <Card className={classes.card}>
@@ -61,29 +65,37 @@ export default function menu() {
       </Typography>
       <CardContent>
         <Typography id="client-name">What is your name?</Typography>
-        <form className={classes.name} autoComplete="off" name="name">
+        <FormControl fullWidth className={classes.name} autoComplete="off" name="name">
           <TextField id="outlined-basic" variant="outlined" />
-        </form>
+        </FormControl>
         <Typography id="client-feeling">How are you feeling now?</Typography>
         <Slider value={feeling} onChange={handleFeelingChange} aria-labelledby="client-feeling" />
         <Typography id="client-mind">In a few words, what's on your mind?</Typography>
-        <TextField
-          id="client-mind-text"
-          aria-labelledby="client-feeling"
-          multiline
-          rows={4}
-          variant="outlined"
-          value={values.problem} onChange={handleChange('problem')}
-        />
+        <FormControl fullWidth id="client-mind-text">
+          <TextField
+            aria-labelledby="client-feeling"
+            multiline
+            rows={4}
+            variant="outlined"
+            value={values.problem}
+            onChange={handleChange('problem')}
+          />
+        </FormControl>
       </CardContent>
       <CardActions>
-        <Button variant="contained" component={Link} to="/">
+        <Button className={classes.button} variant="contained" component={Link} to="/">
           Back
         </Button>
-        <Button variant="contained" onClick={clickSubmit} component={Link} to="/garden">
+        <Button
+          className={classes.button}
+          variant="contained"
+          onClick={clickSubmit}
+          component={Link}
+          to="/garden"
+        >
           Next
         </Button>
       </CardActions>
     </Card>
-  )
+  );
 }
