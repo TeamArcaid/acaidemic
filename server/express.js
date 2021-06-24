bb54466f827f130106f36c6583b790b38ddf631f
In Sang Song <isong231@gmail.com>
2021-06-23 22:41:05 -0400

    Prettier applied to file

--- server/express.js
+++ server/express.js
@@ -1,88 +1,88 @@
-import express from 'express'
-import path from 'path'
-import bodyParser from 'body-parser'
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
 //import cookieParser from' cookie-parser'
-import compress from 'compression'
-import cors from 'cors'
-import helmet from 'helmet'
-import Template from './../template'
-import userRoutes from './routes/user.routes'
-import plantRoutes from './routes/plant.routes'
-import questionRoutes from './routes/question.routes'
+import compress from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+import Template from './../template';
+import userRoutes from './routes/user.routes';
+import plantRoutes from './routes/plant.routes';
+import questionRoutes from './routes/question.routes';
 
 /* modules for server side rendering */
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import MainRouter from './../client/MainRouter'
-import { StaticRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import MainRouter from './../client/MainRouter';
+import { StaticRouter } from 'react-router-dom';
 
-import {ServerStyleSheets, ThemeProvider} from '@material-ui/styles'
-import theme from './../client/theme'
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles';
+import theme from './../client/theme';
 //end
 
-import db from './models'
-db.sequelize.sync()
+import db from './models';
+db.sequelize.sync();
 
 /* comment out before building for production */
-import devBundle from './devBundle'
+import devBundle from './devBundle';
 
-/* ensures Express server properly handles requests to static files 
+/* ensures Express server properly handles requests to static files
 such as CSS files, images or bundled client-side JS */
-const CURRENT_WORKING_DIR = process.cwd()
-const app = express()
+const CURRENT_WORKING_DIR = process.cwd();
+const app = express();
 
 /* comment out before building for production */
-devBundle.compile(app)
+devBundle.compile(app);
 
 /* parse body params and attach them to req.body*/
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(cookieParser())
-app.use(compress())
+app.use(compress());
 /* secure apps by setting various HTTP headers */
-app.use(helmet())
+app.use(helmet());
 /* enable CORS - Cross Origin Resource Sharing*/
-app.use(cors())
+app.use(cors());
 
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 
 /* mount routes */
-app.use('/', userRoutes)
-app.use('/', plantRoutes)
-app.use('/', questionRoutes)
+app.use('/', userRoutes);
+app.use('/', plantRoutes);
+app.use('/', questionRoutes);
 
 app.get('*', (req, res) => {
-    const sheets = new ServerStyleSheets()
-    const context={}
-    const markup = ReactDOMServer.renderToString(
-        sheets.collect(
-            <StaticRouter location={req.url} context={context}>
-                <ThemeProvider theme={theme}>
-                    <MainRouter />
-                </ThemeProvider>
-            </StaticRouter>
-        )
+  const sheets = new ServerStyleSheets();
+  const context = {};
+  const markup = ReactDOMServer.renderToString(
+    sheets.collect(
+      <StaticRouter location={req.url} context={context}>
+        <ThemeProvider theme={theme}>
+          <MainRouter />
+        </ThemeProvider>
+      </StaticRouter>
     )
-    if (context.url){
-        return res.redirect(303, context.url)
-    }
-    const css = sheets.toString()
-    res.status(200).send(Template({
-        markup: markup,
-        css: css
-    }))
-})
-
+  );
+  if (context.url) {
+    return res.redirect(303, context.url);
+  }
+  const css = sheets.toString();
+  res.status(200).send(
+    Template({
+      markup: markup,
+      css: css,
+    })
+  );
+});
 
 /* catch unauthorized errors */
 app.use((err, req, res, next) => {
-    if (err.name === 'UnauthorizedError') {
-      res.status(401).json({"error" : err.name + ": " + err.message})
-    }else if (err) {
-      res.status(400).json({"error" : err.name + ": " + err.message})
-      console.log(err)
-    }
-})
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ error: err.name + ': ' + err.message });
+  } else if (err) {
+    res.status(400).json({ error: err.name + ': ' + err.message });
+    console.log(err);
+  }
+});
 
-
-export default app
+export default app;
