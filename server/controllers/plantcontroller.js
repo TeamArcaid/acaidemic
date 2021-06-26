b8f06c2390e82fbd3ff0b3d7532a8155e9c0f5a1
In Sang Song <isong231@gmail.com>
2021-06-26 18:05:25 -0400

    Ran prettier rules

--- server/controllers/plantcontroller.js
+++ server/controllers/plantcontroller.js
@@ -1,123 +1,120 @@
-import db from '../models'
-const Op = db.Sequelize.Op
+import db from '../models';
+const Op = db.Sequelize.Op;
 
+const createOne = (req, res) => {
+  console.log('create plant api called');
+  const plant = {
+    species: req.body.species,
+    stage: req.body.stage,
+    problem: req.body.problem,
+    startMood: req.body.startMood,
+    endMood: req.body.endMood,
+    userId: '1',
+  };
 
-const createOne = (req, res)=> {
-    console.log('create plant api called')
-    const plant = {
-        species: req.body.species,
-        stage: req.body.stage,
-        problem: req.body.problem,
-        startMood: req.body.startMood,
-        endMood: req.body.endMood,
-        userId: "1"
-    }
-    
-    db.plant.create(plant)
-        .then(data => {
-            res.send(data)
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || 'Some error'
-            })
-        })
-}
+  db.plant
+    .create(plant)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error',
+      });
+    });
+};
 
-const readOne = (req, res)=> {
-    const id = req.params.id
-    db.plant.findByPk(id)
-    .then(data=> {
-        res.send(data)
+const readOne = (req, res) => {
+  const id = req.params.id;
+  db.plant
+    .findByPk(id)
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err=> {
-        res.status(500).send({
-            message: 'Error retrieving Plant with id=' + id
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Error retrieving Plant with id=' + id,
+      });
+    });
+};
 
-const readAll = (req, res)=> {
-    console.log('list plants api called')
-    db.plant.findAll()
-    .then(plants=> {
-        res.json(plants)
-    })
-}
+const readAll = (req, res) => {
+  console.log('list plants api called');
+  db.plant.findAll().then((plants) => {
+    res.json(plants);
+  });
+};
 
+const updateOne = (req, res) => {
+  const id = req.params.id;
 
-const updateOne = (req, res)=> {
-    const id = req.params.id
+  db.plant
+    .update(req.body, {
+      where: { id: id },
+    })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: 'Plant updated',
+        });
+      } else {
+        res.send({
+          message: `Cannot update Plant with id=${id}`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Error updating Plant with id=' + id,
+      });
+    });
+};
 
-    db.plant.update(req.body, {
-        where: { id: id }
+const deleteOne = (req, res) => {
+  const id = req.params.id;
+  db.plant
+    .destroy({
+      where: { id: id },
     })
-    .then(num=> {
-        if(num ==1){
-            res.send({
-                message: 'Plant updated'
-            })
-        } else {
-            res.send({
-                message: `Cannot update Plant with id=${id}`
-            })
-        }
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: 'Plant successfully deleted',
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Plant with id=${id}.`,
+        });
+      }
     })
-    .catch(err => {
-        res.status(500).send({
-            message: 'Error updating Plant with id=' + id
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Could not delete Plant with id=' + id,
+      });
+    });
+};
 
-const deleteOne = (req, res)=> {
-    const id = req.params.id
-    db.plant.destroy({
-        where: {id: id }
+const deleteAll = (req, res) => {
+  db.plant
+    .destroy({
+      where: {},
+      truncate: false,
     })
-    .then(num => {
-        if (num == 1){
-            res.send({
-                message: 'Plant successfully deleted'
-            })
-        } else {
-            res.send({
-                message: `Cannot delete Plant with id=${id}.`
-            })
-        }
+    .then((nums) => {
+      res.send({ message: `${nums} Plants deleted` });
     })
-    .catch(err => {
-        res.status(500).send({
-            message: 'Could not delete Plant with id=' + id
-        })
-    })
-}
-
-const deleteAll = (req, res)=> {
-    db.plant.destroy({
-        where: {},
-        truncate: false
-    })
-    .then(nums=> {
-        res.send({message: `${nums} Plants deleted` })
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: 
-            err.message || 'Some error'
-        })
-    })
-}
-
-
-
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error',
+      });
+    });
+};
 
 export default {
-    createOne,
-    readOne,
-    readAll,
-    updateOne,
-    deleteOne,
-    deleteAll
-}
+  createOne,
+  readOne,
+  readAll,
+  updateOne,
+  deleteOne,
+  deleteAll,
+};
