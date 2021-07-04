2dc036585b3d10c2d97684c335d341db6f6cf14a
In Sang Song <isong231@gmail.com>
2021-07-04 15:20:51 -0400

    Update logger with defaults

--- server/services/logging.js
+++ server/services/logging.js
@@ -1,12 +1,12 @@
-const logBase = (msg, { additionalInfo }) => {
+const logBase = (msg, { ...additionalInfo } = {}) => {
   console.log(msg);
 };
 
 const logError = (msg, { error, ...additionInfo }) => {
   logBase(`Error occured with message: ${msg}. Error Info: ${error}`);
-}
+};
 
-const initializeLogger = (options) => ({
+const initializeLogger = (options = {}) => ({
   log: logBase,
   error: logError,
 });
