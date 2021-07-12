e7bb9a47e4e16264711f889fd23be0d3d172906f
In Sang Song <isong231@gmail.com>
2021-07-11 22:07:51 -0400

    update routes

--- server/routes/response.routes.js
+++ server/routes/response.routes.js
@@ -3,6 +3,8 @@ import responseCtlr from '../controllers/responsecontroller';
 
 const router = express.Router();
 
-router.route('/api/v1/user/:user_id/response').put(responseCtlr.addResponse);
-router.route('/api/v1/user/:user_id/response').get(responseCtlr.getResponses);
+router.route('/api/v1/user/:user_id/response')
+  .put(responseCtlr.addResponse)
+  .get(responseCtlr.getResponses);
+
 export default router;
